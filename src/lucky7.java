7d3d6bdfa22e603585648b7e7a76d9eddc3d1d11
Miika Ståhl <142224301+Prynde@users.noreply.github.com>
2023-10-09 01:47:10 +0300

    Lisätty rahat peliin
    Peli pyörii nyt niin pitkään kuin on rahaa pelata toistorakenteella.

--- src/lucky7.java
+++ src/lucky7.java
@@ -4,17 +4,31 @@ public class lucky7 {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		
-		int number1 = rand.nextInt(10) + 1;
-		int number2 = rand.nextInt(10) + 1;
-		int number3 = rand.nextInt(10) + 1;
-		System.out.println(number1);
-		System.out.println(number2);
-		System.out.println(number3);
-		if (number1 == 7 || number2 == 7 || number3 == 7) {
-			System.out.println("Voitit!");
+		int rahat = 5;
+		int voittoja = 0;
+		int[] voitot = {3, 5, 7};
+		int[] numerot = new int[3];
+		while (rahat >= 1) {
+			rahat--;
+			numerot[0] = rand.nextInt(10) + 1;
+			numerot[1] = rand.nextInt(10) + 1;
+			numerot[2] = rand.nextInt(10) + 1;
+			System.out.println(numerot[0]);
+			System.out.println(numerot[1]);
+			System.out.println(numerot[2]);
+			for ( int i = 0; i < 3; i++) {
+				if (numerot[i] == 7) {
+					voittoja++;
+			}
+		}
+		if (voittoja != 0) {
+			System.out.println("Voitit " + voitot[(voittoja - 1)] + " €!");
+			rahat = rahat + voitot[(voittoja - 1)];
+			voittoja = 0;
 		} else {
 			System.out.println("Hävisit!");
 		}
+		System.out.println("Jäljellä: " + rahat + "€");
 	}
+}
 }
