05d278f23804a337425157510722dd012237a18f
Miika Ståhl <142224301+Prynde@users.noreply.github.com>
2023-10-09 12:54:08 +0300

    Korostettu voittava numero. Korjattu bugi.
    Lisätty ANSI-koodit, punainen tausta ja boldattu valkoinen väri voittavan numeron korostamiseen.
    Korjattu bugi miltä väliltä numeroita arvotaan.

--- src/lucky7.java
+++ src/lucky7.java
@@ -16,6 +16,9 @@ public class lucky7 {
 		int kierros = 0;
 		String uusikierros;
 		String inputclean;
+		String ANSI_RESET = "\u001B[0m";
+		String ANSI_REDBG = "\u001B[41m";
+		String ANSI_WHITE = "\033[1;37m";
 		
 		// Kysytään millä summalla halutaan pelata
 		System.out.println("Syötä rahaa, yksi pelikierros maksaa 1€");
@@ -28,19 +31,36 @@ public class lucky7 {
 			System.out.println("Kierros " + kierros);
 			
 			// Arvotaan numerot
-			numerot[0] = rand.nextInt(10) + 1;
-			numerot[1] = rand.nextInt(10) + 1;
-			numerot[2] = rand.nextInt(10) + 1;
+			numerot[0] = rand.nextInt(9) + 1;
+			numerot[1] = rand.nextInt(9) + 1;
+			numerot[2] = rand.nextInt(9) + 1;
+			// Tarkastetaan voitot ja tulostetaan tulos, voittavat numerot näytetään punaisina
 			System.out.println("===============");
-			System.out.println("|| " + numerot[0] + " | " + numerot[1] + " | " + numerot[2] + " ||");
-			System.out.println("===============");
-			
-			// Tarkastetaan voitot
-			for ( int i = 0; i < 3; i++) {
-				if (numerot[i] == 7) {
-					voittoja++;
-				}
+			System.out.print("|| ");
+			if (numerot[0] == 7) {
+				System.out.print(ANSI_REDBG + ANSI_WHITE + numerot[0] + ANSI_RESET);
+				voittoja++;
+			} else {
+				System.out.print(numerot[0]);
 			}
+			System.out.print(" | ");
+			if (numerot[1] == 7) {
+				System.out.print(ANSI_REDBG + ANSI_WHITE + numerot[1] + ANSI_RESET);
+				voittoja++;
+			} else {
+				System.out.print(numerot[1]);
+			}
+			System.out.print(" | ");
+			if (numerot[2] == 7) {
+				System.out.print(ANSI_REDBG + ANSI_WHITE + numerot[2] + ANSI_RESET);
+				voittoja++;
+			} else {
+				System.out.print(numerot[2]);
+			}
+			System.out.println(" ||");
+			System.out.println("===============");
+						
+			
 			if (voittoja != 0) {
 				System.out.println("Voitit " + voitot[(voittoja - 1)] + " €!");
 				rahat = rahat + voitot[(voittoja - 1)];
