5c63cc6e02a8076f5763865688d489ae4b3386ec
Miika Ståhl <142224301+Prynde@users.noreply.github.com>
2023-10-09 12:14:59 +0300

    Lisätty toiminnallisuutta
    Pelaaja voi syöttää rahamäärän millä pelataan ja kysytään joka kierroksen jälkeen jatketaanko.
    Lisätty kommentteja koodiin.

--- src/lucky7.java
+++ src/lucky7.java
@@ -1,34 +1,66 @@
 import java.io.*;
 import java.util.*;
+
 public class lucky7 {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		int rahat = 5;
+		Scanner in = new Scanner(System.in);
+
+		// Alustetaan kaikki tarvittavat muuttujat
+		int rahat = 0;
 		int voittoja = 0;
 		int[] voitot = {3, 5, 7};
 		int[] numerot = new int[3];
-		while (rahat >= 1) {
+		boolean uusipeli = true;
+		int kierros = 0;
+		String uusikierros;
+		String inputclean;
+		
+		// Kysytään millä summalla halutaan pelata
+		System.out.println("Syötä rahaa, yksi pelikierros maksaa 1€");
+		rahat = in.nextInt();
+		inputclean = in.nextLine(); // Siivotaan syötteestä rivinvaihto ettei looppi pyörähdä alussa kahdesti
+		
+		while (uusipeli == true) {
 			rahat--;
+			kierros++;
+			System.out.println("Kierros " + kierros);
+			
+			// Arvotaan numerot
 			numerot[0] = rand.nextInt(10) + 1;
 			numerot[1] = rand.nextInt(10) + 1;
 			numerot[2] = rand.nextInt(10) + 1;
 			System.out.println(numerot[0]);
 			System.out.println(numerot[1]);
 			System.out.println(numerot[2]);
+			
+			// Tarkastetaan voitot
 			for ( int i = 0; i < 3; i++) {
 				if (numerot[i] == 7) {
 					voittoja++;
+				}
+			}
+			if (voittoja != 0) {
+				System.out.println("Voitit " + voitot[(voittoja - 1)] + " €!");
+				rahat = rahat + voitot[(voittoja - 1)];
+				voittoja = 0;
+			} else {
+				System.out.println("Hävisit!");
+			}
+			// Tarkastetaan onko rahaa vielä jäljellä ja jos on, kysytään pelataanko vielä
+			if (rahat >= 1) {
+				System.out.println("Jäljellä: " + rahat + "€");
+				System.out.println("Haluatko pelata uudelleen? Jos haluat pelata, paina Enter. Jos et halua pelata, kirjoita e ja paina Enter.");
+				uusikierros = in.nextLine();
+				if (uusikierros.equals("e")) {
+					uusipeli = false;
+					System.out.println("Sinulle jäi " + rahat + "€");
+				}
+			} else  {
+				System.out.println("Rahat loppuivat.");
+				uusipeli = false;
 			}
 		}
-		if (voittoja != 0) {
-			System.out.println("Voitit " + voitot[(voittoja - 1)] + " €!");
-			rahat = rahat + voitot[(voittoja - 1)];
-			voittoja = 0;
-		} else {
-			System.out.println("Hävisit!");
-		}
-		System.out.println("Jäljellä: " + rahat + "€");
 	}
-}
 }
